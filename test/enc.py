7dc938a69dbdf1b5b9826494825a1d66f572378b
Josip Delic <delijati@googlemail.com>
2014-04-23 16:23:01 +0200

    added velocity added *.swp to gitignore

--- test/enc.py
+++ test/enc.py
@@ -1,5 +1,12 @@
 import Adafruit_BBIO.GPIO as GPIO
 import config
+import time
+import math
+
+wheel_radius = 1.2  # cm
+time_interval = 1.0  # seconds
+ticks = 20  # holes in disc
+const = (2 * math.pi * wheel_radius)/ticks
 
 
 class EncoderReader(object):
@@ -23,8 +30,23 @@ class EncoderReader(object):
                               callback=self.update_encoder_r)
         GPIO.add_event_detect(config.Ol, GPIO.RISING,
                               callback=self.update_encoder_l)
+        current_time_l = time.time()
+        current_time_r = time.time()
         while True:
-            pass
+            if (time.time() >= current_time_l + time_interval):
+                velocity_l = (
+                    self.counter_l * (wheel_radius * const)
+                ) / time_interval
+                self.counter_l = 0
+                current_time_l = time.time()
+                print "velocity_l %s cm/s" % velocity_l
+            if (time.time() >= current_time_r + time_interval):
+                velocity_r = (
+                    self.counter_r * (wheel_radius * const)
+                ) / time_interval
+                self.counter_r = 0
+                current_time_r = time.time()
+                print "velocity_r %s cm/s" % velocity_r
 
 try:
     enc = EncoderReader()
