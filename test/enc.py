e0a5e394c66a81626443389427f0b0b6ec72b22a
Josip Delic <delijati@googlemail.com>
2014-05-06 13:07:36 +0000

    unified code like in quickbot_bbb

--- test/enc.py
+++ test/enc.py
@@ -7,6 +7,9 @@ wheel_radius = 1.2  # cm
 time_interval = 1.0  # seconds
 ticks = 20  # holes in disc
 const = (2 * math.pi * wheel_radius)/ticks
+LEFT = 0
+RIGHT = 1
+ENC_POS = [0, 0]
 
 
 class EncoderReader(object):
@@ -18,11 +21,15 @@ class EncoderReader(object):
         GPIO.setup(config.Or, GPIO.IN)
 
     def update_encoder_l(self, channel):
-        self.counter_l = self.counter_l + 1
+        global ENC_POS
+        self.counter_l += 1
+        ENC_POS[LEFT] += 1
         #print "Encoder (left) counter updated: %d" % self.counter_l
 
     def update_encoder_r(self, channel):
-        self.counter_r = self.counter_r + 1
+        global ENC_POS
+        self.counter_r += 1
+        ENC_POS[RIGHT] += 1
         #print "Encoder (right) counter updated: %d" % self.counter_r
 
     def run(self):
@@ -39,14 +46,16 @@ class EncoderReader(object):
                 ) / time_interval
                 self.counter_l = 0
                 current_time_l = time.time()
-                print "velocity_l %s cm/s" % velocity_l
+                print "velocity_l %s cm/s ticks sum %s" % (velocity_l,
+                                                           ENC_POS[LEFT])
             if (time.time() >= current_time_r + time_interval):
                 velocity_r = (
                     self.counter_r * (wheel_radius * const)
                 ) / time_interval
                 self.counter_r = 0
                 current_time_r = time.time()
-                print "velocity_r %s cm/s" % velocity_r
+                print "velocity_r %s cm/s ticks sum %s" % (velocity_r,
+                                                           ENC_POS[RIGHT])
 
 try:
     enc = EncoderReader()
